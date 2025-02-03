83b99cf65795b1382aaf4d404fabf8dbe5b386ec
panchal prince <princepanchal1808@gmail.com>
2025-02-03 12:23:40 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // use all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3)    // code redability should be high
+
+console.log("Prince")
+
+
+let name = "Prince"
+let age = 18
+let isLoggedIn = false
+let state;         // undefined
+let stand = null     // represents null value
+
+
+                            // primitive data types
+
+
+// number => 2 to power 53 - 1
+// bigint => 2 to power 53 and above
+// string => " " or ' ' 
+// boolean => true or false
+// null => standalone value or null
+// undefined => value is not assigned
+// symbol => unique identifier
+
+
+                            // object data types
+
+
+console.log(typeof undefined)       // undefined  
+console.log(typeof null)            // object
+ 
