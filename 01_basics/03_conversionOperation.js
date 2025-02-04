26b419fc2dc13c34fcf9e3c1cd66b7caacae0e04
panchal prince <princepanchal1808@gmail.com>
2025-02-04 14:23:52 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valurInNumber = Number(score)
+// console.log(typeof valurInNumber)
+
+// console.log(valurInNumber)
+
+
+// "33" => 33  convert string to number
+// "33abc" => NaN  convert string to number, if string contains any character then it will return NaN, Not a Number, it is a special value in javascript 
+// true => 1  convert boolean to number
+// false => 0  convert boolean to number
+// null => 0  convert null to number
+// undefined => NaN  convert undefined to number
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true  convert number to boolean
+// 0 => false  convert number to boolean
+// "" => false  convert empty string to boolean
+// "Prince" => true  convert string to boolean
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
