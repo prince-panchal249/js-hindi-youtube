b851a8ff0ded440b4dc614acfdfd09d114a305aa
panchal prince <princepanchal1808@gmail.com>
2025-02-10 16:42:16 +0000

    number and maths

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);     // 400
+
+
+const balance = new Number(100);
+console.log(balance);   // [Number: 100]
+
+console.log(balance.toString().length); // 100 => 3
+
+console.log(balance.toFixed(2)); // 100.00
+
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));        
+
+const anotherNumber = 1000000
+console.log(anotherNumber.toLocaleString("en-in")); // 10,00,000
+
+
+
+// Math Object ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
