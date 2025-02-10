598e7dc644c9184b98c82a483d10c477090e082d
panchal prince <princepanchal1808@gmail.com>
2025-02-10 06:26:56 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Prince"
+const repoCount = 3
+
+// console.log(name + repoCount + " repositories") // Prince3 repositories;
+// console.log(name + " has " + repoCount + " repositories") // Princehas3repositories;
+console.log(`${name} has ${repoCount} repositories`) // Prince has 3 repositories;
+
+const gameName = new String("Pubg") // String Object
+// console.log(gameName) // Pubg;
+
+// console.log(gameName[2]);
+console.log(gameName.__proto__);  // String Object
+
+
+console.log(gameName.length); // 4
+console.log(gameName.toUpperCase()); // PUBG
+
