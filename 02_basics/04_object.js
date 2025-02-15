8c9e644090b61f3798a48b799d5f521c064241c2
panchal prince <princepanchal1808@gmail.com>
2025-02-15 06:15:06 +0000

    object part-2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,62 @@
+// const tinderuser = new Object()  // singleton object
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "sammy"
+tinderuser.isLoggedIn = false
+
+// console.log(tinderuser);
+
+const regularUser = {
+    email: "rohitsharma264@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "rohit",
+            lastname: "sharma"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}   // spread method
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "h@gmail.com"
+    },
+    {
+        id: 3,
+        email: "h@gmail.com"
+    },
+]
+
+// console.log(users[1].id);
+// console.log(users.slice(0,2));
+
+users[1].email
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty("isLogged"));
+
+// console.log(tinderuser.toLocaleString());  // don't know the parameter that's why don't print
+
+// console.log(tinderuser.length);
