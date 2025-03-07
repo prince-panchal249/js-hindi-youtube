cba4867c41add8b448d0d755e40af4be767c5439
panchal prince <princepanchal1808@gmail.com>
2025-03-07 17:15:30 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,9 @@
+// for loop
+
+const array = [1, 2, 3, 4, 5, 6]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(`element at index ${index} : ${element}`);
+    // console.log(element);
+}
