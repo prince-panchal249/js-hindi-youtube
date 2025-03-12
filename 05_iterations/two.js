27ba65833c3b14d62c647bc864a07ae08e9c3b9c
panchal prince <princepanchal1808@gmail.com>
2025-03-12 06:06:38 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+// while loop and do-while loop
+
+                                            // while loop
+
+// let index = 0
+// while (index <= 10) {
+    // console.log(`value of index is ${index}`)
+    // index+= 2
+// }
+
+
+
+// let myArray = ["flash", "batman", "superman"]
+// let arr = 0
+
+// while (myArray.length > arr) {
+//     console.log(`value is ${myArray[arr]}`);
+//     // arr++      // or
+//     // arr = arr + 1      // or
+//     arr += 1
+// }
+
+
+                                            // do-while loop                    
+
+let score = 11
+
+do {
+    console.log(`value of score is ${score}`);
+    score++
+} while (score <= 10);
