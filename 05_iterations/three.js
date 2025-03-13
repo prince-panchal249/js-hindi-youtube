7f309d7f44d6afe862cb1c6c6a8bec762bf76f20
panchal prince <princepanchal1808@gmail.com>
2025-03-13 06:45:32 +0000

    for of loop and introduction of map and iteratable of objects

---
+++ 05_iterations/three.js
@@ -0,0 +1,50 @@
+// for of loop
+
+// ["", "", ""] is an array of 3 empty strings
+// for of loop is used to iterate over the values of an iterable object like an array or a string.
+// [{}, {}, {}]
+// for in loop is used to iterate over the keys of an object.
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+    // console.log(`value  is : ${num}`);
+}
+
+
+const greeting = "Hello World!";
+for (const greet of greeting) {
+    // if (greet === " ") {
+    //     break;
+    // }
+    // console.log(`each char is ${greet}`);
+}
+
+
+// Maps 
+
+// Sets are also iterable objects
+
+const map = new Map();  // key value pair    // map is an object that contain key value pairs there is no duplicate keys always unique keys
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+
+for (const [key, values] of map) {
+    // console.log(key, ":-" ,values);
+}
+
+
+
+const myobject = {
+    game1: "Cricket",
+    game2: "Football",
+}
+
+// for (const [key, values] of myobject) {
+//     console.log(key, ":-" ,values);       // error because object is not iterable by for of loop
+// }
